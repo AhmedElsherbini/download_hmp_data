c3e6ef38dd134c6eae88ad31c617505a4417dbd1
Ahmed M. A. Elsherbini <drahmedelsherbini@gmail.com>
2024-04-15 13:44:10 +0200

    Update download_urls.py

--- download_urls.py
+++ download_urls.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jul  17 20:36:39 2022
-
+last update: 15_04_2024
 @author: ahmed elsherbini
+
 """
 #ref:https://medium.com/@petehouston/download-files-with-progress-in-python-96f14f6417a2
 #ref:https://www.programcreek.com/python/example/83386/wget.download
@@ -32,24 +33,35 @@ args = my_parser.parse_args()
 ##################################################
 #step4
 f_name = args.input
-
+#f_name = ("hmp_manifest_1264205a4.tsv")
 ##################################################
 #step5 the analysis 
 manifest_df = pd.read_csv(f_name, sep='\t')
 
 list_of_single_column = manifest_df['urls'].tolist()
 cc=0
+
+print("Tip:In your manifest file.tsv, if the url (https:://......) works in you Internet browser, this scipt will help you.")
+
 for x in list_of_single_column:
     try:
-     a = urlparse(x)
-     cc +=1    
-     print("File number %d" %int(cc))    
-     print(os.path.basename(a.path))
-     wget.download(x)
-     print()
+        if ",ftp://" in x:
+            x = x.split(',')[0]
+            a = urlparse(x)
+            cc +=1    
+            print("File number %d" %int(cc))    
+            print(os.path.basename(a.path))
+            wget.download(x)
+        else:
+           a = urlparse(x)
+           cc +=1    
+           print("File number %d" %int(cc))    
+           print(os.path.basename(a.path))
+           wget.download(x)
     except:
-       print("Erorr in file %s"%(os.path.basename(a.path)))
+     print("Erorr in file %s"%(os.path.basename(a.path)))
+     print("Kindly check on the website of your file is (indeed) avaliable!")
 else:
   print("Finally finished!") 
 ##################################################
-   
+
